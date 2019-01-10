cec8c2be46e9a3ac2692e248adda5b900a87f427
bascoo <b.oosterom@live.nl>
2019-01-10 17:10:16 +0100

    first changes GARCH

--- GARCH.m
+++ GARCH.m
@@ -1,5 +1,18 @@
 clear all 
 clc
 
-%%this is a test
-%%SA test
+%get the values in the Excel using xlsread.
+table           = readtable('netflix_resampled_5minutes.csv');
+datetime        = table2array(table(:,1));
+netflixPrice    = table2array(table(:,2)); 
+
+%%
+
+p               = log(netflixPrice); % log price
+r               = diff(p);           % log return
+mean_r          = mean(r); 
+y               = r - mean_r;
+
+plot(p, 'b')
+hold on
+plot(r, 'r')
