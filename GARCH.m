d728709e9fae54ef675eafc3e4c277060076122c
Sjoerd457 <sjoerd-visser@live.nl>
2019-01-13 13:46:33 +0100

    Update GARCH.m
    Code voor ML

--- GARCH.m
+++ GARCH.m
@@ -21,10 +21,10 @@ plot(r, 'r')
 
 %% 1. Setup
       
-    x = y*1000
+    x = y*1000;
     %Look at last i variables
-    i = 500
-    x = x(end-i:end)
+    i = 500;
+    x = x(end-i:end);
 %     percntiles = prctile(x,[5 95]); %5th and 95th percentile
 %     outlierIndex = x < percntiles(1) | x > percntiles(2);
 %     %remove outlier values
@@ -36,7 +36,7 @@ plot(r, 'r')
 
       omega=0.1;
       alpha=0.1;
-      beta=0.9;
+      beta=0.98;
       
 %% 3. Define Vectors
 
@@ -73,3 +73,68 @@ subplot(2,1,2)
 plot(l)             %plot individual loglikelihood contributions
 line([1 T], [L L])  %draw line of average log likelihood
 xlim([1 T])
+
+
+% %% 1. Setup ML
+% 
+% x = y*1000;
+% 
+% 
+% 
+% x = x(1:5000);
+% %     %Look at last i variables
+% %     i = 500
+% %     x = x(end-i:end);
+% 
+% %% 2. Optimization Options
+% 
+%       options = optimset('Display','iter',... %display iterations
+%                          'TolFun',1e-12,... % function value convergence criteria 
+%                          'TolX',1e-12,... % argument convergence criteria
+%                          'MaxIter',100); % maximum number of iterations    
+% 
+% %% 4. Initial Parameter Values
+%       
+%       omega_ini = 0.1;% initial value for omega
+%       alpha_ini = 0.2;  % initial value for alpha
+%       beta_ini = 0.99;   % initial value for beta
+%       
+%       theta_ini = [omega_ini,alpha_ini,beta_ini];
+%       
+%       
+% %% 5. Parameter Space Bounds
+%         
+%       lb=[0.0001,0,0];    % lower bound for theta
+%       ub=[1000,100,1];   % upper bound for theta
+%       
+%       
+% %% 6. Optimize Log Likelihood Criterion
+%       
+%       % fmincon input:
+%       % (1) negative log likelihood function: - llik_fun_AR1()
+%       % (2) initial parameter: theta_ini
+%       % (3) parameter space bounds: lb & ub
+%       % (4) optimization setup: options
+%       %  Note: a number of parameter restriction are left empty with []
+% 
+%       % fmincon output:
+%       % (1) parameter estimates: theta_hat
+%       % (2) negative average log likelihood value at theta_hat: ls_val
+%       % (3) exit flag indicating (no) convergence: exitflag
+%       
+%       [theta_hat,llik_val,exitflag]=...
+%           fmincon(@(theta) - llik_fun_GARCH(x,theta),theta_ini,[],[],[],[],lb,ub,[],options);
+%       
+%  
+%       
+% %% 7. Print Output
+% 
+% display('parameter estimates:')
+% theta_hat
+% 
+% display('log likelihood value:')
+% -llik_val*(length(x)-1)
+% 
+% display('exit flag:')
+% exitflag %if exitflag>0: convergence ok
+%          %if exitflag<=0: convergence failed
