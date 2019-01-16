5b0f52e1d75dcdcdd42973f03b1dadeb0e994355
bascoo <b.oosterom@live.nl>
2019-01-16 12:14:06 +0100

    fixed run time char to string function

--- char_to_string.m
+++ char_to_string.m
@@ -1,9 +1,7 @@
 function [av]=char_to_string(FF)
-    av = strings([size(FF, 1),1]);
-    for i = 1 : size(FF, 1)
-        for j = 1 : size(FF , 2) 
-            av(i) = strcat(av(i), FF(i,j));
-        end
-    end    
-    av;
+      av = strings([size(FF,1),1]); 
+      for i = 1 : size(FF,1)
+          av(i) = convertCharsToStrings(FF(i,:));
+      end
+      av; 
 end
