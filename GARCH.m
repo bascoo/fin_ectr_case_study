ddcbf77fe144c58259d15e4170d21efc08b62623
sjladams <steven.adams.95@hotmail.com>
2019-01-19 18:31:25 +0100

    -

--- GARCH.m
+++ GARCH.m
@@ -3,3 +3,19 @@ clc
 
 %%this is a test
 %%SA test
+
+%% Load data
+x = readtable('netflix_resampled_5minutes.csv');
+
+%% 
+tableprice = table2array(x(:,2));
+tabledates = table2array(x(:,1));
+%%
+figure(1);clf
+plot(tabledates,tableprice)
+
+%% Garch object
+
+md1 = garch(1,1);
+EstMd1 = estimate(md1
+
--- GAS.m
+++ GAS.m
@@ -1,1 +1,10 @@
 % GAS
+%% Load data
+x = readtable('netflix_resampled_5minutes.csv');
+
+%% 
+tableprice = table2array(x(:,2));
+tabledates = table2array(x(:,1));
+%%
+figure(1);clf
+plot(tabledates,tableprice)
