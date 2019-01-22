c368015878921ce4f489514ad6f9be0db8fb6a4d
bascoo <b.oosterom@live.nl>
2019-01-22 17:46:26 +0100

    start of xi squared estimator for realized kernel

--- Realised_kernel.m
+++ Realised_kernel.m
@@ -3,7 +3,8 @@ clc
 
 %% Load Data
 %get the values in the Excel using xlsread.
-table           = readtable('dNetflix_cleaned_trades_3col.csv');
+table           = readtable('Alcoa4May2007.csv'); % Only one day now
+table           = table(1:end-1,3:4);
 
 %%
 
@@ -14,13 +15,23 @@ table           = readtable('dNetflix_cleaned_trades_3col.csv');
 % for i = 1 : number of days 
 %find and store all obs in day
 
+
 %% each day: 
 
-% FIND BANDWITH
-%-C* , xi_2 , n 
+prices      = table2array(table(:,2));
+log_prices  = log(prices); 
+times       = table2array(table(:,1)); 
 
-% for h = -BANDWITH : BANDWITH
-% k(input) 
+% FIND BANDWIdTH
+c_star  = 3.5134;
+n       = 0;    % TO DO?  ? ? 
+xi_2    = find_xi_2(log_prices,times); 
+
+bandwidth = c_star * xi_2^(2/5) * n^(3/5);
+
+% for h = -BANDWIdTH : BANDWIdTH
+% input = h / (bandwith + 1); 
+% value = Parzen_kernel(input); 
 % gamma_h 
 % Realised_kernal(day) = k(input) * gamma_h
 
