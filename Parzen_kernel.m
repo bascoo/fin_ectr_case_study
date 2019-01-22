9b56732c2452ec40c0f75d2bdfababec6dd054d8
bascoo <b.oosterom@live.nl>
2019-01-22 16:35:14 +0100

    parzen kernel

---
+++ Parzen_kernel.m
@@ -0,0 +1,11 @@
+function [av]=Parzen_kernel(x)
+    if 0 <= abs(x) && abs(x) <= 1/2
+        av = 1 - 6 * (abs(x))^2 + 6 * (abs(x))^3;
+    elseif abs(x) < 1/2 && abs(x) <=1
+        av = 2 * ((1-abs(x))^3); 
+    else 
+        av = 0;
+    end
+    
+    av; 
+end
---
+++ Realised_kernel.m
@@ -0,0 +1,26 @@
+clear all
+clc
+
+%% Load Data
+%get the values in the Excel using xlsread.
+table           = readtable('dNetflix_cleaned_trades_3col.csv');
+
+%%
+
+% find number of days
+% create vector of length arrays
+
+
+% for i = 1 : number of days 
+%find and store all obs in day
+
+%% each day: 
+
+% FIND BANDWITH
+%-C* , xi_2 , n 
+
+% for h = -BANDWITH : BANDWITH
+% k(input) 
+% gamma_h 
+% Realised_kernal(day) = k(input) * gamma_h
+
